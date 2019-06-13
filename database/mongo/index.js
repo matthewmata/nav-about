06ca6cb4d791497e984139cb8e9937e34feb5908
Matthew Mata <matthewmata1030@gmail.com>
2019-06-13 14:14:59 -0700

    fixed url

--- database/mongo/index.js
+++ database/mongo/index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 // 'mongodb://matthewmata1030:password@18.218.132.194/grubhub'
 // 'mongodb://localhost/grubhub'
-mongoose.connect('mongodb://localhost/grubhub', {
+mongoose.connect('mongodb://matthewmata1030:password@18.218.132.194/grubhub', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true
