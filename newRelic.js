c37771f35eddccc12df30c70021fcf4928083780
Matthew Mata <matthewmata1030@gmail.com>
2019-06-14 10:33:56 -0700

    setup new relic

--- newRelic.js
+++ newRelic.js
@@ -5,7 +5,7 @@ exports.config = {
   license_key: '61249e61fff8e1bf595d2afee2e3b9ca750c6d84',
   logging: {
     level: 'trace',
-    filepath: '../../../newrelic_agent.log'
+    filepath: './newrelic_agent.log'
   },
   utilization: {
     detect_aws: false,
