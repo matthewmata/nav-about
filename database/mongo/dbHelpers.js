e21f660bcfaedf9fccb8460509b12caab703cb15
Matthew Mata <matthewmata1030@gmail.com>
2019-06-14 09:05:52 -0700

    updated controller to use raw mongo

--- database/mongo/dbHelpers.js
+++ database/mongo/dbHelpers.js
@@ -1,9 +1,44 @@
+// Mongoose Implementation
 const Restaurant = require('./models');
+// Raw mongo, refactored _id to 1-10000000. mongoose wont query of _id of a preset number
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://matthewmata1030:password@18.218.132.194/grubhub';
+let db;
+
+mongo.connect(url,{ useNewUrlParser: true}, (err, client) => {
+  if (err) {
+    console.error(err);
+    return;
+  } else {
+    db = client.db('grubhub');
+  }
+})
 
 const randomId = () => Math.floor(Math.random() * (10000000 - 9000000 + 1)) + 9000000;
 
 module.exports = {
-  getOneRandom: () => Restaurant.find({ _id: randomId()}),
+  // Mongo
+  getOneRandom: () => new Promise ((resolve, reject) => {
+    db.collection('restaurants').findOne({_id: randomId()}, (err, items) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(items);
+      }
+    });
+  }),
+  getOne: (_id) => new Promise ((resolve, reject) => {
+    db.collection('restaurants').findOne({ _id }, (err, items) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(items);
+      }
+    });
+  }),
+  // Mongoose, _id queries may be broken. Need to refactor to use raw mongo
+  // getOneRandom: () => Restaurant.find({ _id: randomId()}),
+  // getOne: (_id) => Restaurant.find({ _id }).limit(1).maxTimeMS(30),
   postOne: (name, address, phone, schedule, image, priceRange, _id, ratings, categories) => {
     return Restaurant.countDocuments({})
     .then(_id => {
@@ -12,7 +47,6 @@ module.exports = {
     })
   },
   deleteAll: () => Restaurant.deleteMany({}),
-  getOne: (_id) => Restaurant.find({ _id }).limit(1).maxTimeMS(30),
   deleteOne: (_id) => Restaurant.deleteOne({ _id }),
   updateOne: (_id, name, address, phone, schedule, image, priceRange, ratings, categories) => Restaurant.updateOne({ _id }, { name, address, phone, schedule, image, priceRange, ratings, categories })
 }
