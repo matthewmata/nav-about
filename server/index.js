9d57c56fc7cbaaf722f32b76468c7666c09705d4
Matthew Mata <matthewmata1030@gmail.com>
2019-06-14 10:52:38 -0700

    loader.io

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serves static HTML file
-app.use('/loaderio-88dcab11607fc8f41723dfde14be6aec.txt', express.static(path.resolve(__dirname, '../loader.io/loader.txt')))
+app.use('/loaderio-88dcab11607fc8f41723dfde14be6aec.txt', express.static(path.resolve(__dirname, '../loader.io/loaderio-88dcab11607fc8f41723dfde14be6aec.txt')))
 app.use('/', express.static(path.resolve(__dirname, '../public')));
 
 // Router to handle all requests
