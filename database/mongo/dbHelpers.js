96a9df1bf5eb7273b3d50c7497e760247940aa89
Matthew Mata <matthewmata1030@gmail.com>
2019-06-13 20:49:50 -0700

    fixed id

--- database/mongo/dbHelpers.js
+++ database/mongo/dbHelpers.js
@@ -3,16 +3,16 @@ const Restaurant = require('./models');
 const randomId = () => Math.floor(Math.random() * (10000000 - 9000000 + 1)) + 9000000;
 
 module.exports = {
-  getOneRandom: () => Restaurant.find({ id: randomId()}),
-  postOne: (name, address, phone, schedule, image, priceRange, id, ratings, categories) => {
+  getOneRandom: () => Restaurant.find({ _id: randomId()}),
+  postOne: (name, address, phone, schedule, image, priceRange, _id, ratings, categories) => {
     return Restaurant.countDocuments({})
-    .then(id => {
-      id += 1;
-      Restaurant.create({ name, address, phone, schedule, image, priceRange, id, ratings, categories })
+    .then(_id => {
+      _id += 1;
+      Restaurant.create({ name, address, phone, schedule, image, priceRange, _id, ratings, categories })
     })
   },
   deleteAll: () => Restaurant.deleteMany({}),
-  getOne: (id) => Restaurant.find({ id }).limit(1).maxTimeMS(30),
-  deleteOne: (id) => Restaurant.deleteOne({ id }),
-  updateOne: (id, name, address, phone, schedule, image, priceRange, ratings, categories) => Restaurant.updateOne({ id }, { name, address, phone, schedule, image, priceRange, ratings, categories })
+  getOne: (_id) => Restaurant.find({ _id }).limit(1).maxTimeMS(30),
+  deleteOne: (_id) => Restaurant.deleteOne({ _id }),
+  updateOne: (_id, name, address, phone, schedule, image, priceRange, ratings, categories) => Restaurant.updateOne({ _id }, { name, address, phone, schedule, image, priceRange, ratings, categories })
 }
